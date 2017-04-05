3beda4e83cf3ab47a9dcea002f7cd380afe6a4ae
Vluuks <vluuks@gmail.com>
2017-04-06 00:08:16 +0200

    sequence is recognized and separated but not processed well yet

--- 42sh/shell.c
+++ 42sh/shell.c
@@ -21,7 +21,33 @@ void run_command(node_t* node)
     // (for testing:)
     print_tree(node);
 
+	
+	// check if its a built in command
 
+	int pid = fork();	
+	if(pid){
+
+		// if its not 0, then its the parent
+		if(pid < 0){
+			perror("AAAAAAAH");
+			return 1;
+		}
+		else{
+			// something failed
+			// question why is there no fail check for the child? or is this it?
+		}
+	
+	}
+	else{
+
+		// the child code goes here, the child has pid 0
+		// if i understand it correctly the child should do the command that is asked and then exit when its done, allowing the shell itself (the parent) to keep running?
+
+
+	}	
+ 
+	
+	// if its a regular command
 	if (node->type == NODE_COMMAND){
 		
 		char *program = node->command.program;
@@ -32,6 +58,38 @@ void run_command(node_t* node)
 
 
 	}
+	// if its a pipe
+	else if (node->type == NODE_PIPE){
+		printf("pipe?");
+	}
+	// if its a sequence of commands
+	else if (node->type == NODE_SEQUENCE){
+		printf("sequence and shit"); 
+
+		// somehow separate them???
+		// is the max sequence 2, because there are only 2 parts in the struct?
+
+
+		// console.log( kept for future reference to track yearly record of stupidity
+
+
+		// get the partS out I HAVE NO IDEA BUT I GOT THIS RIGHT ON THE FIRST TRY????? WTF
+		char *program = node->sequence.first->command.program;
+		char **argv = node->sequence.first->command.argv;
+
+		// 2nd part of sequence
+		char *programtwo = node->sequence.second->command.program;
+		char **argvtwo = node->sequence.second->command.argv;
+
+		execvp(program, argv);
+		execvp(programtwo, argvtwo);
+	
+
+
+	}
+
+
+
 
     // don't forget:
     free_from_global_mem(node);
